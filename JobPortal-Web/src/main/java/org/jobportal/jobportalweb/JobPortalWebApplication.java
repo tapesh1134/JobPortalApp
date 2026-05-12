58c73b963eaf15350468af45c0b83eb305e73f17
Tapesh Sharma <tapeshs1134@gmail.com>
2026-05-12 10:51:16 +0530

    [tapesh] Add. Add initial setup of CI/CD for Job Portal application with multiple services

---
+++ JobPortal-Web/src/main/java/org/jobportal/jobportalweb/JobPortalWebApplication.java
@@ -0,0 +1,13 @@
+package org.jobportal.jobportalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalWebApplication.class, args);
+    }
+
+}
---
+++ JobPortal-Web/src/test/java/org/jobportal/jobportalweb/JobPortalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jobportal.jobportalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
